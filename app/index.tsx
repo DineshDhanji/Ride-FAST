623e528f27c67a09ddd5a9cb98615e96d0cd45df
Dinesh Dhanji <dhanjeehartik@gmail.com>
2024-12-02 16:41:04 +0500

    Added splash screen and fonts.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Welcome to Ride FAST</Text>
+      <Text style={styles.text}>Well this is just a demo.</Text>
+      <Text style={styles.text}>Parkinsans: Parkinsans</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#09090b",
+    padding: 20,
+  },
+  heading: {
+    color: "#f9fafb",
+    fontSize: 35,
+    marginBottom: 10,
+    fontFamily: "Parkinsans",
+  },
+  text: {
+    color: "#f9fafb",
+    fontSize: 16,
+    fontFamily: "Parkinsans",
+  },
+});
