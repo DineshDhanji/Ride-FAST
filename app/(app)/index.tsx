fc098729f5dcfa89fea975883332cbdee4ba8e2f
Dinesh Dhanji <dhanjeehartik@gmail.com>
2024-12-25 19:39:38 +0500

    demo for Hamza

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,35 +1,40 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { useSession } from "@/session/ctx";
 import { Link } from "expo-router";
-import * as SecureStore from "expo-secure-store";
+import { StatusBar } from "expo-status-bar";
+import { Appearance, useColorScheme } from "react-native";
 
 export default function Index() {
   const { signIn, signOut, session, isLoading } = useSession();
-  
+  let colorScheme = useColorScheme();
+  console.log("colorScheme", colorScheme);
+
   return (
     <View className="flex-1 p-3">
+      <StatusBar />
+
       <Text className="text-zinc-900 text-3xl mb-3">Main App Index</Text>
-      <View className="flex flex-col items-center justify-start  flex-1">
+      <View className="flex flex-col items-center justify-start h-max">
         <Link asChild href={"/_sitemap"}>
-          <TouchableOpacity className="bg-zinc-500 hover:bg-zinc-600 active:bg-zinc-700 text-zinc-50 border border-zinc-950 rounded-lg py-2 px-3 text-center my-1">
-            <Text className="text-zinc-50 text-lg">SiteMap</Text>
+          <TouchableOpacity className="w-full bg-zinc-500 hover:bg-zinc-600 active:bg-zinc-700 text-zinc-50 border border-zinc-950 rounded-lg py-2 px-3 text-center my-1">
+            <Text className="text-zinc-50 text-lg text-center">SiteMap</Text>
           </TouchableOpacity>
         </Link>
         <TouchableOpacity
           onPress={() => {
             signIn();
           }}
-          className="bg-zinc-500 hover:bg-zinc-600 active:bg-zinc-700 text-zinc-50 border border-zinc-950 rounded-lg py-2 px-3 text-center my-1"
+          className="w-full bg-zinc-500 hover:bg-zinc-600 active:bg-zinc-700 text-zinc-50 border border-zinc-950 rounded-lg py-2 px-3 text-center my-1"
         >
-          <Text className="text-zinc-50 text-lg">Sign In</Text>
+          <Text className="text-zinc-50 text-lg text-center">Sign In</Text>
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => {
             signOut();
           }}
-          className="bg-zinc-500 hover:bg-zinc-600 active:bg-zinc-700 text-zinc-50 border border-zinc-950 rounded-lg py-2 px-3 text-center my-1"
+          className="w-full bg-zinc-500 hover:bg-zinc-600 active:bg-zinc-700 text-zinc-50 border border-zinc-950 rounded-lg py-2 px-3 text-center my-1"
         >
-          <Text className="text-zinc-50 text-lg">Sign Out</Text>
+          <Text className="text-zinc-50 text-lg text-center">Sign Out</Text>
         </TouchableOpacity>
       </View>
       <View className="bg-zinc-200 p-3 rounded-lg border border-zinc-950 m-3">
@@ -42,6 +47,17 @@ export default function Index() {
           <Text className="text-lg">{isLoading ? "True" : "False"}</Text>
         </View>
       </View>
+      
+      <View className="bg-zinc-200 p-3 rounded-lg border border-zinc-950 m-3">
+        <Text className="text-xl text-zinc-950">Konnichiiwa</Text>
+      </View>
+      <View className="bg-zinc-800 p-3 rounded-lg border border-zinc-50 m-3">
+        <Text className="text-xl text-zinc-50">Konnichiiwa</Text>
+      </View>
+      <View className="bg-zinc-200 border-zinc-950 dark:bg-zinc-800 dark:border-zinc-50 p-3 rounded-lg border m-3">
+        <Text className="text-xl text-zinc-950 dark:text-zinc-50">Konnichiiwa</Text>
+      </View>
+
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Slot } from "expo-router";
 import { SessionProvider } from "@/session/ctx";
+import { StatusBar } from "expo-status-bar";
 import "@/global.css";
 
 export default function RootLayout() {
   return (
     <SessionProvider>
+      <StatusBar />
       <Slot />
     </SessionProvider>
   );
